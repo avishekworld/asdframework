beef9b0d546e7dcb5df33ca2f53ceceda2504755
avishekahmed <avishek.ahmed@gmail.com>
2015-04-19 14:37:51 -0600

    added gui and uml

--- ASDFrameworkProject/src/Application.java
+++ ASDFrameworkProject/src/Application.java
@@ -1,9 +1,12 @@
+import creditcard.CardFrm;
+import bank.BankFrm;
+
 
 public class Application {
 
 	public static void main(String[] args) {
 
-		System.out.println("Data");
+		new CardFrm().setVisible(true);
 
 	}
 
