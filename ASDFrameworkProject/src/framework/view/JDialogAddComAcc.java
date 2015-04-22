5ac18298706504b536bb53d0568de863a9a2f5bc
Achal <achalachal2003@gmail.com>
2015-04-21 23:54:11 -0500

    Commented out the code where we are setting default values of fields

--- ASDFrameworkProject/src/framework/view/JDialogAddComAcc.java
+++ ASDFrameworkProject/src/framework/view/JDialogAddComAcc.java
@@ -3,48 +3,37 @@ package framework.view;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
-
-
-
-
-
-
 import framework.component.Company;
 import framework.component.CompanyAccountOpenCommand;
-import framework.component.Personal;
-import framework.component.PersonalAccountOpenCommand;
 import framework.component.SimpleTypeFactory;
 
 public class JDialogAddComAcc extends JDialogAddAccount {
 
 	protected JTextField JTextField_NoOfEmp = new JTextField();
 	JLabel JLabel6 = new JLabel();
-	protected SymAction mySymAction= new SymAction();
-	
+	protected SymAction mySymAction = new SymAction();
+
 	public JDialogAddComAcc(AGui myframe) {
 		super(myframe);
-		
+
 		JTextField_NoOfEmp = new JTextField();
-		
-		JTextField_NoOfEmp.setText("8");
+
+		// JTextField_NoOfEmp.setText("8");
 		getContentPane().add(JTextField_NoOfEmp);
-		JTextField_NoOfEmp.setBounds(84,204,156,20);
-		
+		JTextField_NoOfEmp.setBounds(84, 204, 156, 20);
+
 		JLabel6.setText("Total employees");
 		getContentPane().add(JLabel6);
 		JLabel6.setForeground(java.awt.Color.black);
-		JLabel6.setBounds(12,204,96,24);
-		
+		JLabel6.setBounds(12, 204, 96, 24);
+
 		JButton_OK.addActionListener(mySymAction);
 		JButton_Cancel.addActionListener(mySymAction);
-		
-		
+
 	}
-	
-	class SymAction implements java.awt.event.ActionListener
-	{
-		public void actionPerformed(java.awt.event.ActionEvent event)
-		{
+
+	class SymAction implements java.awt.event.ActionListener {
+		public void actionPerformed(java.awt.event.ActionEvent event) {
 			Object object = event.getSource();
 			if (object == JButton_OK)
 				JButtonOK_actionPerformed(event);
@@ -52,31 +41,31 @@ public class JDialogAddComAcc extends JDialogAddAccount {
 				JButtonCalcel_actionPerformed(event);
 		}
 	}
-	
-	public void getCommonInputData()
-	{
+
+	public void getCommonInputData() {
 		super.getCommonInputData();
-		
-		guiData.put(Company.NO_OF_EMPLOYEE_FIELD,JTextField_NoOfEmp.getText());
+
+		guiData.put(Company.NO_OF_EMPLOYEE_FIELD, JTextField_NoOfEmp.getText());
 	}
-	void JButtonOK_actionPerformed(java.awt.event.ActionEvent event)
-	{
+
+	void JButtonOK_actionPerformed(java.awt.event.ActionEvent event) {
 		getCommonInputData();
-		
-		String type=SimpleTypeFactory.getDefaultTypeFactory().getTypes()[0];
-		
-		command=new CompanyAccountOpenCommand(parentframe.getController(), guiData,SimpleTypeFactory.getDefaultTypeFactory().getType(type));
+
+		String type = SimpleTypeFactory.getDefaultTypeFactory().getTypes()[0];
+
+		command = new CompanyAccountOpenCommand(parentframe.getController(),
+				guiData, SimpleTypeFactory.getDefaultTypeFactory()
+						.getType(type));
 		command.exceute();
-		
+
 		parentframe.modelUpdated();
 
 		dispose();
 	}
 
-	void JButtonCalcel_actionPerformed(java.awt.event.ActionEvent event)
-	{
-    //make this frame invisible if Cancel button is clicked
-        dispose();
+	void JButtonCalcel_actionPerformed(java.awt.event.ActionEvent event) {
+		// make this frame invisible if Cancel button is clicked
+		dispose();
 	}
 
 }
